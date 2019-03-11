abaedb302625c3a3598bc5a437b70fe7ea5d4c21
carlosmdarribas <carlosmdarribas@usal.es>
2019-03-11 19:37:11 +0100

    Initial Commit

---
+++ InnoDoc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  InnoDoc
+//
+//  Created by Carlos on 11/03/2019.
+//  Copyright © 2019 Carlos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
