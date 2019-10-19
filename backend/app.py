32749251037463a8a14f8152eb67af15c7af4677
gandalfran <franpintosantos@gmail.com>
2019-10-19 18:51:53 +0200

    solved xss issue

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import logging
 
-from flask import Flask, abort
+from flask import Flask, abort, jsonify
 from flask import request as flask_request
 from flask_cors import CORS, cross_origin
 from flask_swagger_ui import get_swaggerui_blueprint
@@ -19,10 +19,13 @@ from models.organizationsmodel import OrganizationsModel
 
 app = Flask(APP_NAME)
 app.config.from_object('config')
-app.config['SECRET_KEY'] = 'the quick brown kafka jumps over the lazy coder'
-app.config['CORS_HEADERS'] = 'Content-Type'
 
-cors = CORS(app, resources={r"/v1/news": {"origins": "http://t24h.es:50002"}})
+cors = CORS(app, resources={
+    r"/v1/news": {"origins": "http://t24h.es:50003"},
+    r"/v1/heatmap": {"origins": "http://t24h.es:50003"},
+    r"/v1/organizations": {"origins": "http://t24h.es:50003"}
+    }
+)
 
 app.register_blueprint(
     get_swaggerui_blueprint(
@@ -55,11 +58,9 @@ def read_swagger_file():
     with open(SWAGGER_FILE) as f:
         return f.read()
 
-
 ################## SOURCE ##################
 
 @app.route("/v1/heatmap", methods=["GET"])
-@cross_origin(origin='t24h.es',headers=['Content- Type','application/json'])
 def heatmap_endpoint():
     log.info(f"GET {flask_request.path}")
     
@@ -76,11 +77,11 @@ def heatmap_endpoint():
         log.info(f"GET {flask_request.path}: error querying model {ex}")
         return form.build_error_response("internal server error"), 500
 
-    return form.build_response()
-
+    response = jsonify(form.build_response())
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route("/v1/news", methods=["GET"])
-@cross_origin(origin='t24h.es',headers=['Content- Type','application/json'])
 def news_endpoint():
     log.info(f"GET {flask_request.path}")
     
@@ -97,7 +98,9 @@ def news_endpoint():
         log.info(f"GET {flask_request.path}: error querying model {ex}")
         return form.build_error_response("internal server error"), 500
 
-    return form.build_response(news)
+    response = jsonify(form.build_response(news))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route("/v1/organizations", methods=["GET"])
 def organizations_endpoint():
@@ -116,4 +119,6 @@ def organizations_endpoint():
         log.info(f"GET {flask_request.path}: error querying model {ex}")
         return form.build_error_response("internal server error"), 500
 
-    return form.build_response(news)
+    response = jsonify(form.build_response(news))
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
